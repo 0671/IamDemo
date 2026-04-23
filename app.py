383a74d352f4d8cc5720c6ed68044324abbe6db6
0671 <h.vi@qq.com>
2026-04-23 22:03:30 +0800

    Update login view to 'main.login'

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def create_app(config=None):
 
     # 设置 Flask-Login
     login_manager = LoginManager()
-    login_manager.login_view = "login"
+    login_manager.login_view = "main.login"
     login_manager.init_app(app)
     @login_manager.user_loader
     def load_user(user_id):
